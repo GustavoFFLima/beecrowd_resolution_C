50918d56770e04d59575854cc4571bc1e935f5aa
GUSTAVO FERNANDO DE FRAZÃO LIMA <48065631+GustavoFFLima@users.noreply.github.com>
2022-01-26 17:36:05 -0300

    1002
    Resolution in C exercise nº 1002

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main() {
-
-    int  A, B, X;
-    scanf("%d", &A);
-    scanf("%d", &B);
-    X = A + B;
-    printf("X = %d \n", X);
-
+int main()
+{
+    double A, n, R;
+    n = 3.14159;
+    scanf("%lf", &R);
+    A = n * R* R;
+    printf("A=%.4lf \n", A);
+    //o .4 se refere a quantas casas sera mantida depois da virgula
     return 0;
 }
